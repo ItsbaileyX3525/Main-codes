55e711dbf6d72bd5d022ac30ada19e02bfca3657
ItsbaileyX3525 <80989969+ItsbaileyX3525@users.noreply.github.com>
2023-01-31 13:30:23 +0000

    Delete nextbotsV5.6 directory

--- nextbotsV5.6/main2.py
+++
@@ -1,7 +0,0 @@
-from ursina import *
-app=Ursina()
-
-DeathBlood=Entity(model='quad',color=color.red,parent=camera,z=1)
-s1=Sequence(1, Func(DeathBlood.blink,value=color.clear.tint(-.25), duration=1), loop=True)
-s1.start()
-app.run()
