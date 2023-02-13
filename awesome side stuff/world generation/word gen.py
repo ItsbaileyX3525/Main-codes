0cc42a5483bafb32a79360e6c84223f1e56186a3
ItsbaileyX3525 <80989969+ItsbaileyX3525@users.noreply.github.com>
2023-02-13 01:11:51 +0000

    Delete awesome side stuff directory

--- awesome side stuff/world generation/word gen.py
+++
@@ -1,8 +0,0 @@
-from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController
-app=Ursina()
-levels=load_blender_scene(name='worldtest2')
-levels.Landscape.texture = 'texture'
-levels.Landscape.collider = 'box'
-player=FirstPersonController()
-app.run()
